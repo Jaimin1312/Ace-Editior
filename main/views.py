cc8900dfe52bebd64963f491a3e860a5470bd11f
Jaimin1312 <jaiminparmar99999@gmail.com>
2020-09-19 18:43:23 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,18 @@
+  
+import csv,io
+import subprocess
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponse
+from django.http import *
+from .models import *
+from django.contrib import messages
+from django.contrib.auth.models import auth,User
+from django.template import Context, loader
+
+# Create your views here.
+def homepage(request):
+    return render(request,'editor.html')
+
+def submitcode(request):
+    code = request.POST['codevalue']
+    print(code)
+    return redirect('/')
