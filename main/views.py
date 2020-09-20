b4584bfae850ff1d0b705227e5b0504c19bc5795
Jaimin1312 <61569981+Jaimin1312@users.noreply.github.com>
2020-09-20 12:06:39 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -10,9 +10,7 @@ from django.template import Context, loader
 
 # Create your views here.
 def homepage(request):
-    return render(request,'editor.html')
-
-def submitcode(request):
     code = request.POST['codevalue']
     print(code)
-    return redirect('/')
+    return render(request,'editor.html')
+
